1bf94bc224800b2f4e3b438c6f4598602b04539c
isha431 <80724269+isha431@users.noreply.github.com>
2021-07-06 20:10:16 -0400

    .

--- prueba/prueba/urls.py
+++ prueba/prueba/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.conf.urls import include
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
 ]
+
